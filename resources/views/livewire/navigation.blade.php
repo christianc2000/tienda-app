82680dbe34e01ad310ae5a866b54c8f6d401c9a5
christianc2000 <christiancelsomamanisoliz0@gmail.com>
2023-02-21 00:02:43 -0400

    logo

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,11 +1,15 @@
-<header class="bg-violet-700">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center h-16">
-        <a class="flex flex-col items-center justify-center px-4 bg-white bg-opacity-25 text-white cursor-pointer font-semibold h-full">
+<header class="bg-violet-500">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center h-20">
+        <a
+            class="flex flex-col items-center justify-center px-4 bg-white bg-opacity-25 text-white cursor-pointer font-semibold h-full">
             <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                 <path class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M4 6h16M4 12h16M4 18h16" />
             </svg>
             <span>Categorías</span>
-        </a>
+            <a href="/" class="mx-6">
+                {{-- <x-jet-application-mark class="block h-9 w-auto" /> --}}
+                <img src="{{ asset('logo/LOGOTIENDA9.png') }}" alt="" class="block h-16 w-auto">
+            </a>
     </div>
 </header>
